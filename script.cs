a99295be9cc0a8ce77a38f4f13ac5f4903db9cf4
sundehakon <142574795+sundehakon@users.noreply.github.com>
2023-11-10 09:20:25 +0100

    fixed up some issues with comments and code

--- script.cs
+++ script.cs
@@ -1,31 +1,31 @@
-// Her ligger dictionary med en liste over studenter og deres informasjon
+// Oppgave1 og 2 Her ligger dictionary med en liste over studenter og deres informasjon
 Dictionary<string, List<string>> StudentRegister = new Dictionary<string, List<string>>();
-    List<string> StudentInfo1 = new List<string> { "Håkon Sunde", "16", "101" };
+    List<string> StudentInfo1 = new List<string> { "Håkon Sunde", "16", "101"};
     List<string> StudentInfo2 = new List<string> { "Ola Hagen Bendixen", "16", "261" };
     List<string> StudentInfo3 = new List<string> { "John Doe", "17", "398" };
     List<string> StudentInfo4 = new List<string> { "Lebron James", "38", "423" }; 
 
-// Disse er student ideene som brukes til å søke mellom studentene senere i koden, slik at maskinenen kan finne dem i dictionary
+// Disse er student IDene eller keys (nøkkel) som brukes til å søke mellom studentene senere i koden, slik at maskinenen kan finne dem i dictionary
     StudentRegister["101"] = StudentInfo1;
     StudentRegister["261"] = StudentInfo2;
     StudentRegister["398"] = StudentInfo3;
     StudentRegister["423"] = StudentInfo4;
 
-// Et set med forskjellige fagkoder som jeg bruker lenger ned hvor man skal skrive inn karakter til de forskjellige fagene
+// Oppgave3 Et set med forskjellige fagkoder som jeg bruker lenger ned hvor man skal skrive inn karakter til de forskjellige fagene
 HashSet<string> fagkoder = new HashSet<string>();
     fagkoder.Add("ENG");
-    fagkoder.Add("NOR");
+    fagkoder.Add("NOR");r
     fagkoder.Add("MAT");
     fagkoder.Add("NAT");
 
-// Lagde en liste for karakterene brukeren skriver inn lenger ned, som er derfor den er tom
+// Oppgave4 Lagde en liste for karakterene brukeren skriver inn lenger ned, som er derfor den er tom
 List<string> karakterer = new List<string>();
 
 // Ber om studentnummeret, som jeg i den andre if løkken sjekker om samsvarer med noen fra studentregisteret øverst, som er hvorfor vi har linje 9-12
 Console.Write("Skriv inn Student nummer: ");
 string? brukerInput = Console.ReadLine();
 
-// Ekstre feature som spør brukerenen om de ønsker å skrive inn karakterer eller ikke, dersom de sier ja fortsetter koden som vanlig ved å skrive inn karakterer, skriver de nei blir de automatisk sendt til informasjonen om studenten de øsnket å se på, karakterer står da som ikke oppgitt
+// Ekstra feature som spør brukeren om de ønsker å skrive inn karakterer eller ikke, dersom de sier ja fortsetter koden som vanlig ved å skrive inn karakterer, skriver de nei blir de automatisk sendt til informasjonen om studenten de øsnket å se på, karakterer står da som "ikke oppgitt"
 Console.Write("Vil du sette inn karakterer til denne studenten? (y/n): ");
 string? karakterInput = Console.ReadLine();
 
@@ -40,16 +40,16 @@ if (StudentRegister.ContainsKey(brukerInput)) // Sjekker om studentregister har 
     while (true)
         {
 
-        foreach (string fagkode in fagkoder) // Foreach løkke som går gjennom alle fagkodene og ber brukeren skrive inn karakter til disse, verdien fra karakterene som skrives inn blir lagret i listen jeg lagde litt lenger oppe
+        foreach (string fagkode in fagkoder) // Oppgave5 Foreach løkke som går gjennom alle fagkodene og ber brukeren skrive inn karakter til disse, verdien fra karakterene som skrives inn blir lagret i listen jeg lagde litt lenger oppe
             {
                 Console.Write($"{fagkode}: ");
                 string? karakter = Console.ReadLine();
                 karakterer.Add($"{fagkode}: {karakter}");
             }
-            
+
             break;
         }
-// Skriver ut all informasjonen om studenten som ble valgt, inkludert karakterene som akkurat ble gitt
+// Oppgave6 Skriver ut all informasjonen om studenten som ble valgt, inkludert karakterene som akkurat ble gitt
         Console.WriteLine("\nStudent ID: ");
         Console.WriteLine($"Navn: {StudentRegister[brukerInput][0]}");
         Console.WriteLine($"Alder: {StudentRegister[brukerInput][1]}");
@@ -69,10 +69,10 @@ if (StudentRegister.ContainsKey(brukerInput)) // Sjekker om studentregister har 
 } 
 else if (karakterInput.ToLower() == "n".ToLower()) // Sjekker om brukeren skrev inn "n"
 {
-    Console.WriteLine("Student ID: ");
+    Console.WriteLine("\nStudent ID: ");
     Console.WriteLine($"Navn: {StudentRegister[brukerInput][0]}");
     Console.WriteLine($"Alder: {StudentRegister[brukerInput][1]}");
-    Console.WriteLine($"Student nummer: {StudentRegister[brukerInput][2]}");
+    Console.WriteLine($"Student nummer: {StudentRegister[brukerInput][2]}\n");
 
     Console.WriteLine("Karakterer: Ikke oppgitt");
 }
